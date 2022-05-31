53d433b3a28dd4e38574a5b4be71b8f6cb4ba5c8
Impiorum <brettrenelle1980@gmail.com>
2022-05-31 14:17:07 -0600

    Changes by lefnire

---
+++ notes/frontend/src/Home.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Home() {
+    // null means nothing, don't have to return HTML (JSX) necessarily
+    // return null
+
+    return <div>The Homepage</div>
+}
